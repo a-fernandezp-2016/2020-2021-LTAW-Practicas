49939508dc9377391a995f141896d74a1266db79
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-03-02 21:51:08 +0100

    Ejemplo Hola Mundo

---
+++ P1/S2/E1_HolaMundo.js
@@ -0,0 +1,7 @@
+// Primer programa en Javascript Hola Mundo.
+
+// Para imprimir un mensaje en consola, se usa: console.log("¡Hola Mundo!"); .
+
+console.log("¡Hola Mundo!");
+
+console.log(" Bienvenido a mi humilde morada.");
