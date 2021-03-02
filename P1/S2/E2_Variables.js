61341cba45dda032ff5b71e1d409939b9aa6e484
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-03-03 00:13:23 +0100

    Ejemplo Variables

---
+++ P1/S2/E2_Variables.js
@@ -0,0 +1,20 @@
+
+// Programa 2º para definir variables en Javascript.
+
+// Para definir variables, se usa el comando: let <nombre variable> = <valor> .
+
+// Para imprimir mensajes en consola: console.log(""); .
+
+// Para imprimir variable directamente en consola: console.log("", <nombre variable>); .
+
+// Valor de una variable dentro de una cadena: console.log(`Variable n: ${n} metros`);
+
+// Para concatenar la variable al mensaje: console.log("Variable n: " + n);
+
+let var1 = 5;
+
+console.log("La variable var1 vale =", var1);
+
+console.log(`La variable var1 vale = ${var1} metros`);
+
+console.log("La variable var1 tiene un valor de: " + var1);
