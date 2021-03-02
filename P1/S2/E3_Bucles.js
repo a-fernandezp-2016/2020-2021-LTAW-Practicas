6ff6f19827c4775dff2275730700f6f7f3613263
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-03-03 00:31:12 +0100

    Ejemplo Bucles

---
+++ P1/S2/E3_Bucles.js
@@ -0,0 +1,15 @@
+//  3º programa en Javascript, relacionado con bucles.
+
+//  Para definir ctes, se pone: const <nombre_cte> = valor; .
+
+// Bucle for se hace así: for(i=0; i<N; i++) {
+//      }
+
+const Num_Max = 15;
+
+console.log("Empieza la cuenta hacia delante.....\n");
+
+for(i=1; i<=Num_Max; i++)
+{
+    console.log(` ${i} `);
+}
