5e807e9edb7d13d3ee16ddcf749700675411b1ab
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-03-19 00:34:08 +0100

    Modulos URLs

---
+++ P1/S3/ModuloURL.js
@@ -0,0 +1,7 @@
+// Declarar módulo url para trabajar con urls que solicita un cliente o clientes.
+const url = require('url');
+
+// la clase URL que es la que me permite crear objetos de tipo URL, con los que trabajar. 
+// Accedemos a esa clase de esta forma:
+const URL = url.URL;
+
