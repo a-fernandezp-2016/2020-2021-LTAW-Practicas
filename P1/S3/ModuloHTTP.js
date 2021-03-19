022873d10c9edc976924bc6782f1c13f73d54b16
a-fernandezp-2016 <a.fernandezp.2016@alumnos.urjc.es>
2021-03-19 19:18:54 +0100

    Modified Modulo HTTP

--- P1/S3/ModuloHTTP.js
+++ P1/S3/ModuloHTTP.js
@@ -1,3 +1,12 @@
 // Declarar módulo HTTP para trabajar con Cliente y Servidor.
 const http = require('http');
 
+// Declarar módulo url para trabajar con urls que solicita un cliente o clientes.
+const url = require('url');
+
+// Declarar módulo stream de entrada. Los streams son objetos de node.js que nos permiten enviar y recibir datos.
+const stream = require('stream');
+
+// Para convertirla a una URL completa, 
+// y poder así utilizar el módulo URL para acceder a todos sus campos, hacemos los siguiente:
+const myURL = new URL(req.url, 'http://' + req.headers['host']);
